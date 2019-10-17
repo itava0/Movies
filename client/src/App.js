d833366d3a5286d673c3ffc0b3a1e647a3899332
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-17 18:31:10 -0400

    Created a Router to update movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateForm from "./Movies/UpdateForm";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(res => setItems(res.data))
+      .catch(error => console.log(error));
+  }, []);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -18,7 +27,13 @@ const App = () => {
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} updateItems={setItems}/>;
+        }}
+      />
+      <Route
+        path="/update-movie/:id"
+        render={props => {
+          return <UpdateForm {...props} items={items} updateItems={setItems}/>;
         }}
       />
     </>
